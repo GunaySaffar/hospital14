a21497fdfc4b2ded44a54f4b5ee72bca52015eb2
Gunay <gunayava4776@gmail.com>
2017-09-14 13:16:49 +0400

    Add files via upload

---
+++ Xestexana-qebul/Xestexana-qebul/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Xestexana_qebul
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sobe Elave Edin";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(10, 73);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(146, 33);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "Elave Et";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 129);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(144, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "Sobeler";
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(10, 179);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(66, 35);
+            this.DeleteButton.TabIndex = 5;
+            this.DeleteButton.Text = "Sil";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // EditButton
+            // 
+            this.EditButton.Location = new System.Drawing.Point(82, 179);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(74, 35);
+            this.EditButton.TabIndex = 6;
+            this.EditButton.Text = "Deish";
+            this.EditButton.UseVisualStyleBackColor = true;
+            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(10, 235);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 301);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button EditButton;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Xestexana-qebul/Xestexana-qebul/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Xestexana_qebul
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Hospital;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            var sobeAd = textBox1.Text;
+            var sql = "insert into sobe(ad) values('" + sobeAd + "')";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            var selectSql = "select * from Sobe";
+            var selectCmd = new SqlCommand(selectSql, conn);
+            var adapter = new SqlDataAdapter(selectCmd);
+            var ds = new DataSet();
+            adapter.Fill(ds);
+            comboBox1.Items.Clear();
+            for (var i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                comboBox1.Items.Add(ds.Tables[0].Rows[i]["ad"]);
+            }
+            conn.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Hospital;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            var selectSql = "select * from Sobe";
+            var selectCmd = new SqlCommand(selectSql, conn);
+            var adapter = new SqlDataAdapter(selectCmd);
+            var ds = new DataSet();
+            adapter.Fill(ds);
+            comboBox1.Items.Clear();
+            for (var i=0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                comboBox1.Items.Add(ds.Tables[0].Rows[i]["ad"]);
+            }
+            conn.Close();
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Hospital;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            var selectedInd = comboBox1.SelectedIndex;
+            var sql = "delete from Sobe where id=" + selectedInd + "";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            var selectSql = "select * from Sobe";
+            var selectCmd = new SqlCommand(selectSql, conn);
+            var adapter = new SqlDataAdapter(selectCmd);
+            var ds = new DataSet();
+            adapter.Fill(ds);
+            comboBox1.Items.Clear();
+            for (var i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                comboBox1.Items.Add(ds.Tables[0].Rows[i]["ad"]);
+                comboBox1.Items.Remove(comboBox1.SelectedItem);
+            }
+            conn.Close();
+        }
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Hospital;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            var selectedInd = comboBox1.SelectedIndex;
+            var sql = "update Sobe set   where id=" + selectedInd + "";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            var selectSql = "select * from Sobe";
+            var selectCmd = new SqlCommand(selectSql, conn);
+            var adapter = new SqlDataAdapter(selectCmd);
+            var ds = new DataSet();
+            adapter.Fill(ds);
+            comboBox1.Items.Clear();
+            for (var i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                comboBox1.Items.Add(ds.Tables[0].Rows[i]["ad"]);
+                comboBox1.Items.Remove(comboBox1.SelectedItem);
+            }
+            conn.Close();
+        }
+    }
+}
